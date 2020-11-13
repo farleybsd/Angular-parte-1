4240a18c2639f788844d1012090327bf99255e1d
farleybsd <farley.t.i@hotmail.com>
2020-11-12 21:47:24 -0300

    Change Detection

--- alurapic/src/app/Photos/photo-list/photos/photos.component.ts
+++ alurapic/src/app/Photos/photo-list/photos/photos.component.ts
@@ -1,18 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Photo } from '../../photo/photo';
+import { Photo } from './../../photo/photo';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
 
 @Component({
   selector: 'ap-photos',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   @Input() photos: Photo[] = []; // passa parametro vindo de outro component
+  rows: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+ ngOnChanges(changes: SimpleChanges)
+ {
+   if(changes.photos)
+      this.rows = this.groupColumns(this.photos);
+ }
+
+  groupColumns(photos: Photo[])
+  {
+      const newRows = [];
+      for(let index = 0; index < photos.length; index+=3) {
+        newRows.push(photos.slice(index, index + 3));
+
+    }
+    console.log(newRows);
+      return newRows;
   }
 
 }
